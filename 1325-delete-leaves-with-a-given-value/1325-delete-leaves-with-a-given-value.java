140f2823c2c0fee29fbfa385fca4abe2e906ac41
Jiapei1001 <jiapeili@live.com>
2022-01-05 16:29:48 -0800

    Time: 0 ms (100.00%), Space: 42.2 MB (5.47%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -20,9 +20,9 @@ class Solution {
             return root;
         }
         
-        if (root.left == null && root.right == null && root.val == target) {
-            return null;
-        }
+        // if (root.left == null && root.right == null && root.val == target) {
+        //     return null;
+        // }
         
         root.left = removeLeafNodes(root.left, target);
         root.right = removeLeafNodes(root.right, target);
