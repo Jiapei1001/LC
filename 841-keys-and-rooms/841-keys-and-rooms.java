56859951f7be7f5d7da3cf85b443564f1bd4864c
Jiapei1001 <jiapeili@live.com>
2022-01-11 13:42:32 -0800

    Time: 5 ms (17.51%), Space: 42 MB (11.82%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.java
@@ -0,0 +1,68 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        if (rooms == null || rooms.size() == 0) return true;
+        
+        int n = rooms.size();
+        
+        Set<Integer> visited = new HashSet<>();
+        visited.add(0);
+        return dfs(rooms, 0, visited);
+    }
+    
+    private boolean dfs(List<List<Integer>> rooms, int curr, Set<Integer> visited) {
+        if (visited.size() == rooms.size()) {
+            return true;
+        }
+        
+        for (int next : rooms.get(curr)) {
+            if (visited.contains(next)) {
+                continue;
+            }
+            
+            visited.add(next);
+            if (dfs(rooms, next, visited)) {
+                return true;
+            }
+            // no need to remove
+        }
+        
+        return false;
+    }
+    
+    
+    
+    
+    
+    
+    
+    /*
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        
+        int n = rooms.size();
+        boolean[] visited = new boolean[n];
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(rooms, i, visited);
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) return false;
+        }
+        
+        return true;
+    }
+    
+    private void dfs(List<List<Integer>> rooms, int curr, boolean[] visited) {
+        if (visited[curr]) {
+            return;
+        }
+        
+        visited[curr] = true;
+        for (int next : rooms.get(curr)) {
+            dfs(rooms, next, visited);
+        }
+    }
+    */
+}
