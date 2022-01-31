a3b848712e379bf791fd7af6d74b59f1267d1c98
Jiapei1001 <jiapeili@live.com>
2022-01-30 22:23:57 -0800

    Time: 28 ms (32.36%), Space: 51 MB (11.69%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.java
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.java
@@ -1,7 +1,7 @@
 class Solution {
     
     private static int MOD = (int) Math.pow(10, 9) + 7;
-    /*
+
     public int numWays(int steps, int arrLen) {
 //         f(i, j) := the number of ways at postion j, for the front ith number of steps
         
@@ -11,17 +11,19 @@ class Solution {
         
 //         f(0, j) = 1, as initial step, where 0 <= j <= arrLen - 1
 //         f(i, 0) = 0
-            
-        long[] dp = new long[arrLen + 1];
+        
+        int maxPos = Math.min(steps, arrLen);
+        
+        long[] dp = new long[maxPos];
         
         // base case: step as 0, at position of 0 (index of 0)
         dp[0] = 1;
         
         for (int i = 1; i <= steps; i++) {
-            long[] next = new long[arrLen + 1];
-            for (int j = 0; j < arrLen; j++) {                
+            long[] next = new long[maxPos];
+            for (int j = 0; j < maxPos; j++) {                
                 next[j] = (dp[j] +
-                           dp[j + 1] +
+                           ((j + 1) <= maxPos - 1 ? (dp[j + 1]) : 0) +
                            ((j - 1) >= 0 ? (dp[j - 1]) : 0)) % MOD;
             }
             dp = next;
@@ -29,23 +31,7 @@ class Solution {
         
         return (int) dp[0];
     }
-    */
     
-    public int numWays(int steps, int arrLen) {
-        int maxPos = Math.min(steps,arrLen);
-        final int MOD = (int) 1e9+7;
-        long[] dp = new long[maxPos+1]; //holds the previous step's state
-        dp[0]=1;
-        
-        for(int i = 1; i <= steps; i++) {
-            long[] temp = new long[maxPos+1];
-            for(int j = 0; j < maxPos; j++) 
-                temp[j] = (dp[j] + dp[j+1] + (j>0?dp[j-1]:0)) % MOD;
-            dp = temp; 
-        }
-        
-        return (int)dp[0];
-    }
     
     // Recursion 正确，但是会Memory Limit Exceeded！！
     /*
