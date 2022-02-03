a79d213814330af4ac870e9fb8929c405a8f19a1
JP <jiapeili@live.com>
2022-02-03 00:35:13 -0800

    Time: 1 ms (62.05%), Space: 41.9 MB (15.84%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
@@ -1,4 +1,37 @@
 class Solution {
+
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        int n = books.length;
+        
+        // f(i, j) := the minimal height for the front i-th books, with width of j
+        // current level, looking back, find how many books can be put on the current level
+        // f(i, j) = f(k, shelfWidth) + Max Height from book k -> i
+            
+        
+        int[] dp = new int[n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            int currBookW = books[i - 1][0];
+            int currBookH = books[i - 1][1];
+
+            dp[i] = dp[i - 1] + currBookH;
+
+            // current level, looking back
+            int accWidth = currBookW;
+            int currMaxHeight = currBookH;
+            for (int k = i - 1; k >= 1 && accWidth + books[k - 1][0] <= shelfWidth; k--) {
+                currMaxHeight = Math.max(currMaxHeight, books[k - 1][1]);
+                accWidth += books[k - 1][0];
+
+                dp[i] = Math.min(dp[i], currMaxHeight + dp[k - 1]);
+            }
+        }
+        
+        return dp[n];
+    }
+    
+    // Top Down
+    /*
     public int minHeightShelves(int[][] books, int shelfWidth) {
         //Note that at each step of the above process, the order of the books we place is the same order as the given sequence of books.
         // 这一句决定了books的顺序是一个接着一个，不是无序的，因此可以缩小问题的规模，使用DP
@@ -40,4 +73,5 @@ class Solution {
         
         return memo[idx][currW];
     }
+    */
 }
