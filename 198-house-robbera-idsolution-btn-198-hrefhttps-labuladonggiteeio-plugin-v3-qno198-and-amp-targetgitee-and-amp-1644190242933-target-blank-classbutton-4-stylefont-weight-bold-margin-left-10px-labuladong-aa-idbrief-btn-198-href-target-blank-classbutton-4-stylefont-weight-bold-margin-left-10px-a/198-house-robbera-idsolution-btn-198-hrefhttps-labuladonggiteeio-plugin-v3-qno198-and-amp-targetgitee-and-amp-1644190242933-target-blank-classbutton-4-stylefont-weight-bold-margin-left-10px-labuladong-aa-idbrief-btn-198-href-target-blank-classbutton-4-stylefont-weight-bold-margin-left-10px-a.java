4e3a28f7761aa1f20960fbd21a49506f01822ac8
JP <jiapeili@live.com>
2022-02-06 15:36:16 -0800

    Time: 0 ms (100.00%), Space: 41.4 MB (10.48%) - LeetHub

--- 198-house-robbera-idsolution-btn-198-hrefhttps-labuladonggiteeio-plugin-v3-qno198-and-amp-targetgitee-and-amp-1644190242933-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-198-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/198-house-robbera-idsolution-btn-198-hrefhttps-labuladonggiteeio-plugin-v3-qno198-and-amp-targetgitee-and-amp-1644190242933-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-198-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
+++ 198-house-robbera-idsolution-btn-198-hrefhttps-labuladonggiteeio-plugin-v3-qno198-and-amp-targetgitee-and-amp-1644190242933-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-198-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/198-house-robbera-idsolution-btn-198-hrefhttps-labuladonggiteeio-plugin-v3-qno198-and-amp-targetgitee-and-amp-1644190242933-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-198-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -5,11 +5,11 @@ class Solution {
         int[] memo = new int[n + 1];
         Arrays.fill(memo, -1);
         
-        return dfs(nums, memo, 1, n);
+        return dfs(nums, memo, 1);
     }
     
-    private int dfs(int[] nums, int[] memo, int s, int e) {
-        if (s > e) {
+    private int dfs(int[] nums, int[] memo, int s) {
+        if (s > nums.length) {
             return 0;
         }
         
@@ -18,8 +18,8 @@ class Solution {
         }
         
         int res = 0;
-        res = Math.max(dfs(nums, memo, s + 1, e),                   // don't take
-                       dfs(nums, memo, s + 2, e) + nums[s - 1]);    // take
+        res = Math.max(dfs(nums, memo, s + 1),                   // don't take
+                       dfs(nums, memo, s + 2) + nums[s - 1]);    // take
         
         memo[s] = res;
         return res;
