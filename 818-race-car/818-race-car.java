85a3bb24ad32bc85cb9a58bfd24e4247d1d4e35a
JP <jiapeili@live.com>
2022-02-06 22:10:24 -0800

    Time: 584 ms (8.29%), Space: 135.7 MB (23.62%) - LeetHub

---
+++ 818-race-car/818-race-car.java
@@ -0,0 +1,59 @@
+class Solution {
+    
+    private String[] dirs = {"A", "R"};
+    
+    class Car {
+        int pos;
+        int speed;
+        
+        public Car(int pos, int speed) {
+            this.pos = pos;
+            this.speed = speed;
+        }
+    }
+    
+    public int racecar(int target) {
+        if (target == 0) {
+            return 0;
+        }
+        
+        // int[] {position, spend}
+        
+        Queue<int[]> q = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        
+        int[] origin = new int[]{0, 1};
+        q.offer(origin);
+        visited.add(0 + "/" + 1);
+        
+        int level = 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                
+                if (curr[0] == target) {
+                    return level;
+                }
+                
+                String s1 = (curr[0] + curr[1]) + "/" + curr[1] * 2;
+                String s2 = curr[0] + "/" + (curr[1] > 0 ? -1 : 1);
+                
+                if (Math.abs(curr[0] + curr[1] - target) < target && !visited.contains(s1)) {
+                    q.offer(new int[]{curr[0] + curr[1], curr[1] * 2});
+                    visited.add(s1);
+                }
+                
+                if (Math.abs(curr[0] - target) < target && !visited.contains(s2)) {
+                    q.offer(curr[1] > 0 ? new int[]{curr[0], -1} : new int[]{curr[0], 1});
+                    visited.add(s2);
+                }
+            }
+            
+            level++;
+        }
+        
+        return -1;
+    }
+}
