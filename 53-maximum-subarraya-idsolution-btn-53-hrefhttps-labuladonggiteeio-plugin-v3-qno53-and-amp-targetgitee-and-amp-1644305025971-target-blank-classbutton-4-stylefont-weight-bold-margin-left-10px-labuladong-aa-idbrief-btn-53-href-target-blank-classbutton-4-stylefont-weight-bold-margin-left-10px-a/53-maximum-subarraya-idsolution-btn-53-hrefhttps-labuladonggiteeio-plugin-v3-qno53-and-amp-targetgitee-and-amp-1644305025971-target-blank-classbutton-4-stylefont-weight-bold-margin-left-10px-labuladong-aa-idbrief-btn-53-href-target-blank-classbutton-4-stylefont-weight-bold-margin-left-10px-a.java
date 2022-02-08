59df224da64b0125c97b345910f768509e1587c8
JP <jiapeili@live.com>
2022-02-07 23:29:49 -0800

    Time: 3 ms (26.70%), Space: 74.3 MB (53.30%) - LeetHub

--- 53-maximum-subarraya-idsolution-btn-53-hrefhttps-labuladonggiteeio-plugin-v3-qno53-and-amp-targetgitee-and-amp-1644305025971-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-53-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/53-maximum-subarraya-idsolution-btn-53-hrefhttps-labuladonggiteeio-plugin-v3-qno53-and-amp-targetgitee-and-amp-1644305025971-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-53-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
+++ 53-maximum-subarraya-idsolution-btn-53-hrefhttps-labuladonggiteeio-plugin-v3-qno53-and-amp-targetgitee-and-amp-1644305025971-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-53-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/53-maximum-subarraya-idsolution-btn-53-hrefhttps-labuladonggiteeio-plugin-v3-qno53-and-amp-targetgitee-and-amp-1644305025971-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-53-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -1,4 +1,25 @@
 class Solution {
+    
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        
+        for (int i = 1; i < n; i++) {
+            dp[i] = nums[i] + (dp[i - 1] < 0 ? 0 : dp[i - 1]);
+        }
+        
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        
+        return res;
+    }
+    
+    
+    /*
     public int maxSubArray(int[] nums) {
         int res = Integer.MIN_VALUE;
         int acc = 0;
@@ -11,4 +32,5 @@ class Solution {
         
         return res;
     }
+    */
 }
