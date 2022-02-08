dee3174c5918d980642c200791b49fa7ad93f935
JP <jiapeili@live.com>
2022-02-07 23:47:06 -0800

    Time: 38 ms (5.24%), Space: 115.4 MB (5.32%) - LeetHub

--- 121-best-time-to-buy-and-sell-stocka-idsolution-btn-121-hrefhttps-labuladonggiteeio-plugin-v3-qno121-and-amp-targetgitee-and-amp-1644305424186-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-121-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/121-best-time-to-buy-and-sell-stocka-idsolution-btn-121-hrefhttps-labuladonggiteeio-plugin-v3-qno121-and-amp-targetgitee-and-amp-1644305424186-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-121-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
+++ 121-best-time-to-buy-and-sell-stocka-idsolution-btn-121-hrefhttps-labuladonggiteeio-plugin-v3-qno121-and-amp-targetgitee-and-amp-1644305424186-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-121-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/121-best-time-to-buy-and-sell-stocka-idsolution-btn-121-hrefhttps-labuladonggiteeio-plugin-v3-qno121-and-amp-targetgitee-and-amp-1644305424186-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-121-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -7,18 +7,45 @@ class Solution {
         // f(i, 1) = Math.max(f(i - 1, 1), f(i - 1, 0) - prices[i])
         // f(i, 0) = Math.max(f(i - 1, 0), f(i - 1, 1) + prices[i])
         
+        int dpi_0 = 0;
+        int dpi_1 = Integer.MIN_VALUE;
+            
+        for (int i = 1; i <= n; i++) {
+            // NOTE: 这里千万不能是dp[i - 1][0] - prices[i - 1]
+            // 因为这样代表着无限可能！！
+            // dp[i][1] = Math.max(dp[i - 1][1], 0 - prices[i - 1]);
+            int temp = dpi_1;
+            dpi_1 = Math.max(dpi_1, - prices[i - 1]);
+            // dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
+            dpi_0 = Math.max(dpi_0, temp + prices[i - 1]);
+        }
+        
+        return dpi_0;
+    }
+    
+    
+    /*
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        
+        int[][] dp = new int[n + 1][2];
+        
+        // f(i, 1) = Math.max(f(i - 1, 1), f(i - 1, 0) - prices[i])
+        // f(i, 0) = Math.max(f(i - 1, 0), f(i - 1, 1) + prices[i])
+        
         dp[0][0] = 0;
         dp[0][1] = Integer.MIN_VALUE;
             
         for (int i = 1; i <= n; i++) {
             // NOTE: 这里千万不能是dp[i - 1][0] - prices[i - 1]
             // 因为这样代表着无限可能！！
-            dp[i][1] = Math.max(dp[i - 1][1], - prices[i - 1]);
+            dp[i][1] = Math.max(dp[i - 1][1], 0 - prices[i - 1]);
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
         }
         
         return dp[n][0];
     }
+    */
     
     
     /*
