6798182851e3505f909a50be914113300ee11e71
JP <jiapeili@live.com>
2022-02-07 23:44:27 -0800

    Time: 51 ms (5.24%), Space: 110.2 MB (5.32%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stocka-idsolution-btn-121-hrefhttps-labuladonggiteeio-plugin-v3-qno121-and-amp-targetgitee-and-amp-1644305424186-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-121-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/121-best-time-to-buy-and-sell-stocka-idsolution-btn-121-hrefhttps-labuladonggiteeio-plugin-v3-qno121-and-amp-targetgitee-and-amp-1644305424186-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-121-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        
+        int[][] dp = new int[n + 1][2];
+        
+        // f(i, 1) = Math.max(f(i - 1, 1), f(i - 1, 0) - prices[i])
+        // f(i, 0) = Math.max(f(i - 1, 0), f(i - 1, 1) + prices[i])
+        
+        dp[0][0] = 0;
+        dp[0][1] = Integer.MIN_VALUE;
+            
+        for (int i = 1; i <= n; i++) {
+            // NOTE: 这里千万不能是dp[i - 1][0] - prices[i - 1]
+            // 因为这样代表着无限可能！！
+            dp[i][1] = Math.max(dp[i - 1][1], - prices[i - 1]);
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
+        }
+        
+        return dp[n][0];
+    }
+    
+    
+    /*
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        
+        int[] leftMin = new int[n];
+        leftMin[0] = prices[0];
+        for (int i = 1; i < n; i++) {
+            leftMin[i] = Math.min(leftMin[i - 1], prices[i]);
+        }
+        
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = prices[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], prices[i]);
+        }
+        
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (rightMax[i] > leftMin[i]) {
+                res = Math.max(res, rightMax[i] - leftMin[i]);
+            }
+        }
+        
+        return res;
+    }
+    */
+}
