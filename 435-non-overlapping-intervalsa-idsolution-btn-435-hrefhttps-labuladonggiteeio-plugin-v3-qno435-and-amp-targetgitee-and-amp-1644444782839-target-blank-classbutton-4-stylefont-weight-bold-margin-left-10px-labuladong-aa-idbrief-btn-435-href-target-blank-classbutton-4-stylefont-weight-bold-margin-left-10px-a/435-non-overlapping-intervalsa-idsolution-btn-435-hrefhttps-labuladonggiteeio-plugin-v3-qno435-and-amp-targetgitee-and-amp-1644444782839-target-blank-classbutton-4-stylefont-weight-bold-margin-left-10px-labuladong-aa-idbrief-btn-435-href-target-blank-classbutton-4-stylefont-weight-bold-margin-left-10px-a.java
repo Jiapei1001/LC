6c2312c80944dea078c32347caf9ec4f0573af52
JP <jiapeili@live.com>
2022-02-09 14:21:21 -0800

    Time: 107 ms (30.97%), Space: 100 MB (59.42%) - LeetHub

---
+++ 435-non-overlapping-intervalsa-idsolution-btn-435-hrefhttps-labuladonggiteeio-plugin-v3-qno435-and-amp-targetgitee-and-amp-1644444782839-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-435-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/435-non-overlapping-intervalsa-idsolution-btn-435-hrefhttps-labuladonggiteeio-plugin-v3-qno435-and-amp-targetgitee-and-amp-1644444782839-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-435-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+        
+        int res = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
+                intervals[i][1] = Math.min(intervals[i][1], intervals[i - 1][1]);
+                res++;
+            }
+        }
+        
+        return res;
+    }
+}
