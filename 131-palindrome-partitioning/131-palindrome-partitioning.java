396ec11a3298f0362a988a852dcbd96a7401a15a
JP <jiapeili@live.com>
2022-02-08 19:07:25 -0800

    Time: 9 ms (79.44%), Space: 54.1 MB (74.83%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -2,11 +2,53 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<>();
         
+        if (s == null || s.length() == 0) {
+            return res;
+        }
+        
+        int n = s.length();
+        boolean[][] memo = new boolean[n][n];
+        // base case
+        for (int i = 0; i < n; i++) memo[i][i] = true;
+        
+        List<String> path = new ArrayList<>();
+        dfs(s, 0, path, res, memo);
+        
+        return res;
+    }
+    
+    private void dfs(String s, int i, List<String> path,
+                     List<List<String>> res, boolean[][] memo) {
+        if (i == s.length()) {
+            res.add(new ArrayList<String>(path));
+            return;
+        }
+        
+        char[] sArr = s.toCharArray();
+        
+        // [i, j]
+        for (int j = i; j < s.length(); j++) {
+           if (j == i || (sArr[i] == sArr[j] && (j == i + 1 || memo[i + 1][j - 1]))) {
+               memo[i][j] = true;
+               
+               String temp = s.substring(i, j + 1);
+               path.add(temp);
+               dfs(s, j + 1, path, res, memo);
+               path.remove(path.size() - 1);
+           }
+        }
+    }
+    
+    
+    // Backtracking
+    /*
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        
         if (s == null || s.length() == 0) return res;
         
         // check if it is palindrome
         // reach the end of the string
-        
         List<String> path = new ArrayList<>();
         
         dfs(s, 0, path, res);
@@ -50,4 +92,5 @@ class Solution {
         
         return true;
     }
+    */
 }
