40c539a9a8f66ec54ab95192f2028569e5b131fd
JP <jiapeili@live.com>
2022-02-10 16:35:31 -0800

    Time: 242 ms (54.73%), Space: 213.6 MB (61.56%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,3 +1,5 @@
+// TreeMap + floorKey
+/*
 class TimeMap {
     Map<String, TreeMap<Integer, String>> map;
 
@@ -24,6 +26,58 @@ class TimeMap {
         return keyMap.get(floorKey);
     }
 }
+*/
+
+class TimeMap {
+    Map<String, List<Data>> map;
+
+    public TimeMap() {
+        this.map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        this.map.computeIfAbsent(key, a -> new ArrayList<Data>()).add(new Data(timestamp, value));
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!this.map.containsKey(key)) {
+            return "";
+        }
+        
+        List<Data> list = this.map.get(key);
+        return this.binarySearchLessThan(list, timestamp);
+    }
+    
+    private String binarySearchLessThan(List<Data> list, int target) {
+        int l = 0, r = list.size() - 1;
+        
+        while (l + 1 < r) {
+            int mid = l + (r - l) / 2;
+            
+            if (list.get(mid).time <= target) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+        
+        if (list.get(r).time <= target) return list.get(r).val;
+        if (list.get(l).time <= target) return list.get(l).val;
+        
+        return "";
+    }
+}
+
+class Data {
+    int time;
+    String val;
+    
+    public Data (int time, String val) {
+        this.time = time;
+        this.val = val;
+    }
+}
+
 
 /**
  * Your TimeMap object will be instantiated and called as such:
