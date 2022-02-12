2e34c5d693dcd05540e24caff48448421a8ea0ca
JP <jiapeili@live.com>
2022-02-12 13:14:57 -0800

    Time: 7 ms (38.97%), Space: 43.7 MB (5.21%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        // increasing stack,
+        // when is not the last appeared index, pop
+        // if it is the last appeared index, don't pop
+        int n = s.length();
+        char[] sArr = s.toCharArray();
+        
+        int[] lastIdx = new int[26];
+        
+        int uniqueCnt = 0;
+        for (int i = 0; i < n; i++) {
+            lastIdx[sArr[i] - 'a'] = i;
+        }
+        
+        Stack<Integer> stack = new Stack<>();
+        Set<Character> visited = new HashSet<>();
+        
+        for (int i = 0; i < n; i++) {
+            if (visited.contains(sArr[i])) {
+                continue;
+            }
+            
+            while (!stack.isEmpty() && 
+                   (// increasing stack
+                    sArr[i] <= sArr[stack.peek()] && 
+                    // make sure it is not the last char
+                    i < lastIdx[sArr[stack.peek()] - 'a'])) {
+                int last = stack.pop();
+                visited.remove(sArr[last]);
+            }
+            
+            visited.add(sArr[i]);
+            stack.push(i);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) sb.append(sArr[stack.pop()]);
+        
+        return sb.reverse().toString();
+    }
+}
