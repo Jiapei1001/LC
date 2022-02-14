4514eb1012f55640aa63b61cf54c6e81c25db400
JP <jiapeili@live.com>
2022-02-14 14:51:01 -0800

    Time: 42 ms (70.88%), Space: 108.1 MB (52.65%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxResult(int[] nums, int k) {
+//         f(j) = MAX{f(i) + nums[j]}, where j - k <= i <= j - 1
+            
+//         we want to get the max sum of the available values in the k window
+        
+//         decresing order, strict, as the newer one is closer to future state
+        
+        int n = nums.length;
+        int[] dp = new int[n];
+        
+        Deque<int[]> monoq = new ArrayDeque<>();
+        dp[0] = nums[0];
+        monoq.offer(new int[]{0, dp[0]});
+        
+        for (int i = 1; i < n; i++) {
+            // NOTE: 因为是连续的，因此即使之前的dp[prev] < 0，也要相加
+            dp[i] = nums[i] + monoq.peekFirst()[1];
+            
+            // make sure the max distance is k - 1,
+            // to make this (k - 1) window available when i becomes i + 1
+            while (!monoq.isEmpty() && monoq.peekFirst()[0] + k <= i) {
+                monoq.pollFirst();
+            }
+            while (!monoq.isEmpty() && monoq.peekLast()[1] <= dp[i]) {
+                monoq.pollLast();
+            }
+            
+            monoq.offer(new int[]{i, dp[i]});
+        }
+        
+        return dp[n - 1];
+    }
+}
