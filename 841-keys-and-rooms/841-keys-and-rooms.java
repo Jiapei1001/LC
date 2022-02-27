66696b50f9f7a40c0651a3ac358497317d691e82
JP <jiapeili@live.com>
2022-02-26 17:16:00 -0800

    Time: 1 ms (96.09%), Space: 45.2 MB (11.16%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.java
+++ 841-keys-and-rooms/841-keys-and-rooms.java
@@ -1,4 +1,32 @@
 class Solution {
+    
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n = rooms.size();
+        boolean[] visited = new boolean[n];
+        
+        dfs(rooms, 0, visited);
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) return false;
+        }
+        
+        return true;
+    }
+    
+    private void dfs(List<List<Integer>> rooms, int i, boolean[] visited) {
+        visited[i] = true;
+        
+        for (int j : rooms.get(i)) {
+            if (!visited[j]) dfs(rooms, j, visited);
+        }
+    }
+    
+    
+    
+    
+    
+    
+    /*
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         if (rooms == null || rooms.size() == 0) return true;
         
@@ -28,6 +56,7 @@ class Solution {
         
         return false;
     }
+    */
     
     
     
