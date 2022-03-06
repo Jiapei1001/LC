7bcb3b0207742b5c6e0b44f5209dc00535932aee
JP <jiapeili@live.com>
2022-03-05 21:12:52 -0800

    Time: 2 ms (82.44%), Space: 42.2 MB (22.07%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.java
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.java
@@ -1,6 +1,38 @@
 class Solution {
+
+    public int mctFromLeafValues(int[] arr) {
+        int n = arr.length;
+        
+        // monotonic stack, decreasing
+        // if larger, than update sum, 
+        // \/, we want to use the middle one * the smaller one on left or right
+            
+        Stack<Integer> stack = new Stack<>();
+        stack.push(Integer.MAX_VALUE);
+        
+        int res = 0;
+        
+        for (int i = 0; i < n; i++) {
+            while (stack.size() > 1 && stack.peek() < arr[i]) {
+                int mid = stack.pop();
+                res += mid * Math.min(stack.peek(), arr[i]);
+            }
+            
+            stack.push(arr[i]);
+        }
+        
+        int last = stack.pop();
+        while(stack.size() > 1) {
+            int temp = stack.pop();
+            res += temp * last;
+            last = temp;
+        }
+        
+        return res;
+    }
     
     
+    /*
     public int mctFromLeafValues(int[] arr) {
         int n = arr.length;
         
@@ -24,12 +56,7 @@ class Solution {
         
         return res;
     }
-    
-    
-    
-    
-    
-    
+    */
     
     
     /*
