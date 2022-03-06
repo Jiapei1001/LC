fd424e485b1ef9e9b57fd858387eca73b1400f43
JP <jiapeili@live.com>
2022-03-05 16:55:10 -0800

    Time: 28 ms (91.30%), Space: 51 MB (87.42%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,7 +1,38 @@
 class Solution {
     
     private int MOD = (int) 1e9 + 7;
+
+    public int numSubseq(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int n = nums.length;
+        
+        Arrays.sort(nums);
+        
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = 2 * dp[i - 1] % MOD;
+        }
+        
+        int l = 0, r = n - 1;
+        
+        long res = 0;
+        while (l <= r) {
+            if (nums[l] + nums[r] > target) {
+                r--;
+            } else {
+                res = (res + dp[r - l]) % MOD;
+                l++;
+            }
+        }
+        
+        return (int) res;
+    }
     
+    /*
     public int numSubseq(int[] nums, int target) {
         // [1,2,3] 3 + 2 + 1
         // [1,2,3,4] 4 + 3 + 2 + 1
@@ -44,6 +75,7 @@ class Solution {
                 // int m = r - l;
                 // long subArrayCount = (long) Math.pow(2, m);
                 
+                // r - l代表不算第一个数的长度，因为subArrayCount代表以第一个l开始的subsequences
                 int subArrayCount = pows[r - l];
                 res = (res + subArrayCount) % MOD;
                 
@@ -55,4 +87,5 @@ class Solution {
         
         return (int) res % MOD;
     }
+    */
 }
