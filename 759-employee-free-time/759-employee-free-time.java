e611c9c71c9f2cc257875efd5048c740bb39f94a
JP <jiapeili@live.com>
2022-03-05 18:01:10 -0800

    Time: 11 ms (85.68%), Space: 54.3 MB (43.53%) - LeetHub

--- 759-employee-free-time/759-employee-free-time.java
+++ 759-employee-free-time/759-employee-free-time.java
@@ -16,6 +16,54 @@ class Interval {
 class Solution {
     
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        // combine as a single timeline
+        // sort by start time, then by end time larger first
+        // 1. [0,3], [0,2]
+        // 2. [0,3], [1,4]
+        // 3, [0,3], [5,6]
+        
+        List<Interval> list = new ArrayList<>();
+        
+        if (schedule == null || schedule.size() == 0) {
+            return new ArrayList<>();
+        }
+        
+        for (List<Interval> i : schedule) {
+            list.addAll(i);
+        }
+        
+        Collections.sort(list, (a, b) -> {
+            if (a.start == b.start) {
+                return b.end - a.end;
+            } else {
+                return a.start - b.start;
+            }
+        });
+        
+        // [1,3], [1,2], [4, 10] [5,6]
+        
+        List<Interval> res = new ArrayList<>();
+        LinkedList<Interval> merge = new LinkedList<>();
+        for (Interval i : list) {
+            if (merge.isEmpty()) {
+                merge.add(i);
+                continue;
+            }
+            
+            if (i.start <= merge.getLast().end) {
+                merge.getLast().end = Math.max(i.end, merge.getLast().end);
+            } else {
+                res.add(new Interval(merge.getLast().end, i.start));
+                merge.addLast(i);
+            }
+        }
+        
+        return res;
+    }
+    
+    
+    /*
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
         // merge intervals
         // pq, pop() with peek()
         
@@ -51,6 +99,7 @@ class Solution {
         
         return res;
     }
+    */
     
     
     
